6dc6e3643aa3ede9149d51d8dbdd95407a5b332f
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-12 19:08:18 +0530

    dicomkit: model[ScaleImage]

--- dicomkit/model.py
+++ dicomkit/model.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+class ScaleImage(object):
+    '''
+    - Scale The Dicom Image
+    '''
+    def __init__(self, image, width, height):
+        self.image = image
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return '[%dx%d]' % (self.width, self.height)
+
