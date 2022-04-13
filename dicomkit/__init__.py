b8200bf7a3ef78ae674f70293459b5338a45d570
Krisna Pranav <krisna.pranav@gmail.com>
2022-04-13 19:28:48 +0530

    dicomkit: __init__

--- dicomkit/__init__.py
+++ dicomkit/__init__.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import os 
+import png
+import numpy as np
+import pydicom
+from .model import ScaleImage
+from .contrast import contrast_auto
+
+def mri_to_png(mri_file, png_file, do_auto_contrast=False):
+    image_2d = extract_grayscale_image(mri_file)
+
+    if do_auto_contrast:
+        image_2d = contrast_auto(image_2d)
+    
+    w = png.Writer(image_2d.width, image_2d.height, greyscale=True)
+    w.write(png_file, image_2d.image)
+
+def extract_grayscale_image(mri_file):
+    plan = pydicom.read_file(mri_file)
